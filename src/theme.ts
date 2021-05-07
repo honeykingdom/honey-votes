0c62609200c7085cb5d7c74950abe5680c412b0f
DmitryScaletta <dmitryscaletta@mail.ru>
2021-05-07 18:07:54 +0300

    ✨ Add material ui-theme

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { ThemeOptions } from "@material-ui/core/styles";
+
+const theme: ThemeOptions = {
+  palette: {
+    mode: "dark",
+  },
+  typography: {
+    htmlFontSize: 16,
+  },
+};
+
+export default theme;
