b374443362ee2db44f542c4223f4424246dca01c
DmitryScaletta <dmitryscaletta@mail.ru>
2021-05-07 18:04:44 +0300

    🔧 Add next seo config

---
+++ next-seo.config.ts
@@ -0,0 +1,12 @@
+import type { DefaultSeoProps } from "next-seo";
+
+const config: DefaultSeoProps = {
+  title: "HoneyVotes",
+  description: "HoneyVotes",
+  openGraph: {
+    type: "website",
+    locale: "ru_RU",
+  },
+};
+
+export default config;
