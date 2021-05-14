effaea5735ce5af43fad6e9ec89669e866953b8f
DmitryScaletta <dmitryscaletta@mail.ru>
2021-05-14 22:34:41 +0300

    ✨ Add arrow-right icon

---
+++ src/icons/arrow-right.svg.tsx
@@ -0,0 +1,7 @@
+const ArrowRightIcon = (props: any) => (
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+    <path strokeWidth="4" d="M2 3v26l26-13z" />
+  </svg>
+);
+
+export default ArrowRightIcon;
