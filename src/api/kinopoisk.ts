79a4aca4ff30dc382b291f62b4d898fb336e866e
DmitryScaletta <dmitryscaletta@mail.ru>
2021-05-14 22:31:33 +0300

    ✨ Add kinopoisk api

---
+++ src/api/kinopoisk.ts
@@ -0,0 +1,42 @@
+const BASE_URL = "https://kinopoiskapiunofficial.tech/api/v2.1/";
+
+const headers = {
+  Accept: "application/json",
+  "X-API-KEY": process.env.NEXT_PUBLIC_KINOPOISK_API_KEY,
+};
+
+export type KinopoiskMovie = {
+  filmId: number;
+  nameRu: string;
+  nameEn?: string;
+  type?: string;
+  year?: string;
+  description?: string;
+  filmLength?: string;
+  countries?: {
+    country: string;
+  }[];
+  genres?: {
+    genre: string;
+  }[];
+  rating?: string;
+  ratingVoteCount?: number;
+  posterUrl?: string;
+  posterUrlPreview?: string;
+};
+
+export type SearchByKeywordResponse = {
+  keyword: string;
+  pagesCount: number;
+  films: KinopoiskMovie[];
+  searchFilmsCountResult: number;
+};
+
+export const searchByKeyword = async (keyword: string) => {
+  const response = await fetch(
+    `${BASE_URL}films/search-by-keyword?keyword=${keyword}`,
+    { headers }
+  );
+
+  return response.json() as unknown as SearchByKeywordResponse;
+};
