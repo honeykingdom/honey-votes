714b81571c0de189b238a5faa4b8d347d176c36d
DmitryScaletta <dmitryscaletta@mail.ru>
2021-08-08 00:06:01 +0300

    🔧 Use next-transpile-modules plugin

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 const withPWA = require("next-pwa");
+const withTM = require("next-transpile-modules")([
+  "d3-array",
+  "internmap",
+  "d3-ease",
+]);
 
-module.exports = withPWA({
-  pwa: {
-    disable: process.env.NODE_ENV === "development",
-    dest: "public",
-  },
-});
+module.exports = withTM(
+  withPWA({
+    pwa: {
+      disable: process.env.NODE_ENV === "development",
+      dest: "public",
+    },
+  })
+);
