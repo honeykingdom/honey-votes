d50d3b785e606c57e7e7d889bb31dfee2c45f634
DmitryScaletta <dmitryscaletta@mail.ru>
2021-09-20 15:25:21 +0300

    ✨ Add Badge component

---
+++ src/components/Badge.tsx
@@ -0,0 +1,26 @@
+import { Typography } from "@mui/material";
+import globalBadges from "features/twitch-api/globalBadges.mock.json";
+
+type Props = {
+  name: string;
+  version?: string;
+  children?: React.ReactNode;
+};
+
+const Badge = ({ name, version = "1", children }: Props) => {
+  const badge = globalBadges.badge_sets[name].versions[version];
+
+  return (
+    <Typography variant="inherit" color="inherit" component="span">
+      <img
+        src={badge.image_url_1x}
+        srcSet={`${badge.image_url_2x} 2x, ${badge.image_url_4x} 4x`}
+        alt={name}
+        style={{ marginRight: 4, verticalAlign: "middle" }}
+      />
+      {children}
+    </Typography>
+  );
+};
+
+export default Badge;
