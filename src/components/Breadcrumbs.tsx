906f9e45a919f9369c7d55ad4fe4237992e0d9a5
DmitryScaletta <dmitryscaletta@mail.ru>
2021-09-20 15:25:38 +0300

    ✨ Add Breadcrumbs component

---
+++ src/components/Breadcrumbs.tsx
@@ -0,0 +1,33 @@
+import NextLink from "next/link";
+import { Breadcrumbs as MuiBreadcrumbs, Link, Typography } from "@mui/material";
+
+type Breadcrumb = {
+  title: string | React.ReactNode;
+  href?: string;
+};
+
+type Props = {
+  items: Breadcrumb[];
+};
+
+const Breadcrumbs = ({ items }: Props) => {
+  return (
+    <MuiBreadcrumbs aria-label="breadcrumb">
+      {items.map(({ title, href }, key) =>
+        href ? (
+          <NextLink href={href} passHref key={key}>
+            <Link underline="hover" color="inherit">
+              {title}
+            </Link>
+          </NextLink>
+        ) : (
+          <Typography color="text.primary" key={key}>
+            {title}
+          </Typography>
+        )
+      )}
+    </MuiBreadcrumbs>
+  );
+};
+
+export default Breadcrumbs;
