fcdd3f64a040dc97043ffae9288d2c9c545e8a05
DmitryScaletta <dmitryscaletta@mail.ru>
2021-10-02 12:13:10 +0300

    🏷️ Add twitch api types

---
+++ src/features/twitch-api/twitch.ts
@@ -0,0 +1,16 @@
+export type TwitchUser = {
+  id: string;
+  login: string;
+  display_name: string;
+  type: string;
+  broadcaster_type: string;
+  description: string;
+  profile_image_url: string;
+  offline_image_url: string;
+  view_count: number;
+  created_at: string;
+};
+
+export type TwitchUsersResponse = {
+  data: TwitchUser[];
+};
