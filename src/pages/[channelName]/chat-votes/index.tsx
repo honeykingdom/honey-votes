337c7766d4f3d7721c6b21a849a0f4564feac84b
DmitryScaletta <dmitryscaletta@mail.ru>
2021-10-08 13:29:47 +0300

    ✨ Add chat-votes page

---
+++ src/pages/[channelName]/chat-votes/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "components/Layout";
+import ChatVotesPage from "features/chat-votes/pages/ChatVotesPage";
+
+const ChatVotes = () => {
+  return (
+    <Layout>
+      <ChatVotesPage />
+    </Layout>
+  );
+};
+
+export default ChatVotes;
