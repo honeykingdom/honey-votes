1cceacd76892517a84fbb6ed56bb20de6b43d15b
DmitryScaletta <dmitryscaletta@mail.ru>
2021-10-18 20:10:09 +0300

    ✨ Add api constants

---
+++ src/features/api/constants.ts
@@ -0,0 +1,8 @@
+export const API_BASE = `${process.env.NEXT_PUBLIC_API_DOMAIN_URL}/api/honey-votes`;
+export const API_BASE_POSTGREST = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1`;
+
+export const USER_TABLE_NAME = "hv_user";
+export const VOTING_TABLE_NAME = "hv_voting";
+export const VOTING_OPTION_TABLE_NAME = "hv_voting_option";
+export const CHAT_VOTING_TABLE_NAME = "hv_chat_voting";
+export const CHAT_VOTE_TABLE_NAME = "hv_chat_vote";
