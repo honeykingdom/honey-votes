6fe42f50ad09399f985a0031affa225745d9a7bb
DmitryScaletta <dmitryscaletta@mail.ru>
2021-10-18 20:44:00 +0300

    ✨ Add purple button component

---
+++ src/components/PurpleButton.tsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+import { Button, darken } from "@mui/material";
+
+const PurpleButton = styled(Button)`
+  background-color: #6441a5;
+
+  &:hover {
+    background-color: ${darken("#6441a5", 0.1)};
+  }
+
+  &:active {
+    background-color: ${darken("#6441a5", 0.2)};
+  }
+`;
+
+export default PurpleButton;
