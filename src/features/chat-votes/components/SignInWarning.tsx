d642118c448f32a0258cdb573d6b3a5a6cc409c4
DmitryScaletta <dmitryscaletta@mail.ru>
2021-10-19 17:10:21 +0300

    ✨ Add SignInWarning component

---
+++ src/features/chat-votes/components/SignInWarning.tsx
@@ -0,0 +1,32 @@
+import { Alert, AlertTitle, Link } from "@mui/material";
+import { useMeQuery } from "features/api/apiSlice";
+import useChannelLogin from "hooks/useChannelLogin";
+import { AUTH_URL, LS_REDIRECT_PATH } from "utils/constants";
+
+const SignInWarning = () => {
+  const login = useChannelLogin();
+  const me = useMeQuery();
+
+  const handleSignIn = () => {
+    localStorage.setItem(LS_REDIRECT_PATH, window.location.pathname);
+  };
+
+  return (
+    <Alert severity="warning">
+      <AlertTitle>
+        Голосование для канала <strong>{login}</strong> ещё не создано.
+      </AlertTitle>
+      Если вы владелец канала <strong>twitch.tv/{login}</strong>, пожалуйста,{" "}
+      {me.isSuccess ? (
+        "войдите"
+      ) : (
+        <Link href={AUTH_URL} onClick={handleSignIn}>
+          войдите
+        </Link>
+      )}
+      , чтобы создать голосование.
+    </Alert>
+  );
+};
+
+export default SignInWarning;
