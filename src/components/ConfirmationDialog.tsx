c046eb10102ade9d85f29199c9331cc9baf8821a
DmitryScaletta <dmitryscaletta@mail.ru>
2021-10-19 17:04:35 +0300

    ✨ Add ConfirmationDialog component

---
+++ src/components/ConfirmationDialog.tsx
@@ -0,0 +1,52 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+
+type Props = {
+  open: boolean;
+  title?: string;
+  description?: string;
+  handleClose: () => void;
+  handleYes: () => void;
+};
+
+const ConfirmationDialog = ({
+  open,
+  title,
+  description,
+  handleYes,
+  handleClose,
+}: Props) => (
+  <Dialog
+    open={open}
+    onClose={handleClose}
+    aria-labelledby="alert-dialog-title"
+    aria-describedby="alert-dialog-description"
+  >
+    <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+    <DialogContent>
+      <DialogContentText id="alert-dialog-description">
+        {description}
+      </DialogContentText>
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={handleClose}>Нет</Button>
+      <Button
+        onClick={() => {
+          handleYes();
+          handleClose();
+        }}
+        autoFocus
+      >
+        Да
+      </Button>
+    </DialogActions>
+  </Dialog>
+);
+
+export default ConfirmationDialog;
