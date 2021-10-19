c245c24c16e265909d447c95d549b78b26d5995e
DmitryScaletta <dmitryscaletta@mail.ru>
2021-10-19 20:42:29 +0300

    ✨ Add storeTokens function

---
+++ src/utils/storeTokens.ts
@@ -0,0 +1,8 @@
+import { LS_ACCESS_TOKEN, LS_REFRESH_TOKEN } from "./constants";
+
+const storeTokens = (accessToken: string, refreshToken: string) => {
+  localStorage.setItem(LS_ACCESS_TOKEN, accessToken);
+  localStorage.setItem(LS_REFRESH_TOKEN, refreshToken);
+};
+
+export default storeTokens;
