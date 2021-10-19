78931d63af48fc0e00ad4af0f1841884ec71f952
DmitryScaletta <dmitryscaletta@mail.ru>
2021-10-19 17:11:46 +0300

    ✨ Move ChatVotes page to the pages folder

--- src/pages/[channelLogin]/chat-votes/index.tsx
+++ src/pages/[channelLogin]/chat-votes/index.tsx
@@ -1,12 +1,45 @@
+import Head from "next/head";
+import { Typography, Box, Divider } from "@mui/material";
 import Layout from "components/Layout";
-import ChatVotesPage from "features/chat-votes/pages/ChatVotesPage";
+import Breadcrumbs from "components/Breadcrumbs";
+import { useUserQuery } from "features/api/apiSlice";
+import ChatVotingComponent from "features/chat-votes/components/ChatVotingComponent";
+import SignInWarning from "features/chat-votes/components/SignInWarning";
+import useChannelLogin from "hooks/useChannelLogin";
 
-const ChatVotes = () => {
+const ChatVotesPage = () => {
+  const login = useChannelLogin();
+  const channel = useUserQuery({ login }, { skip: !login });
+
   return (
     <Layout>
-      <ChatVotesPage />
+      <Head>
+        {/* TODO: " - Голосование в чате" */}
+        <title>{channel.data?.displayName || login} - Голосование в чате</title>
+      </Head>
+
+      <Typography variant="h4" component="div" sx={{ mb: 2 }}>
+        Голосование в чате
+      </Typography>
+
+      <Box sx={{ mb: 2 }}>
+        <Breadcrumbs
+          items={[
+            {
+              title: channel.data?.displayName || login,
+              // href: `/${channel.data?.login || login}`,
+            },
+            { title: "Голосование в чате" },
+          ]}
+        />
+      </Box>
+
+      <Divider sx={{ mb: 2 }} />
+
+      <ChatVotingComponent />
+      {channel.isSuccess && !channel.data && <SignInWarning />}
     </Layout>
   );
 };
 
-export default ChatVotes;
+export default ChatVotesPage;
