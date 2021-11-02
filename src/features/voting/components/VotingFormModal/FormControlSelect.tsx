5e2ccc2b764cf4faef9c88fa820505701dddfe45
DmitryScaletta <dmitryscaletta@mail.ru>
2021-11-02 14:46:00 +0300

    ✨ Add FormControlSelect component

---
+++ src/features/voting/components/VotingFormModal/FormControlSelect.tsx
@@ -0,0 +1,33 @@
+import { FormControl, InputLabel, Select } from "@mui/material";
+import { Control, Controller } from "react-hook-form";
+
+type Props = {
+  id: string;
+  name: string;
+  label: string;
+  control: Control;
+  children: any;
+};
+
+const FormControlSelect = ({ id, name, label, control, children }: Props) => (
+  <FormControl variant="standard" sx={{ display: "flex" }}>
+    <InputLabel id={`${id}-select-label`}>{label}</InputLabel>
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => (
+        <Select
+          label={label}
+          labelId={`${id}-select-label`}
+          id={`${id}-select`}
+          size="small"
+          {...field}
+        >
+          {children}
+        </Select>
+      )}
+    />
+  </FormControl>
+);
+
+export default FormControlSelect;
