d4675867ef02364c8ec0369fc02cbb9789fd87e1
DmitryScaletta <dmitryscaletta@mail.ru>
2021-11-02 14:17:14 +0300

    ✨ Add VotingList component

---
+++ src/features/voting/components/VotingList.tsx
@@ -0,0 +1,27 @@
+import { Grid } from "@mui/material";
+import { Voting } from "features/api/types";
+import VotingCard from "./VotingCard";
+
+type Props = {
+  canManage: boolean;
+  votingList: Voting[];
+  channelLogin: string;
+};
+
+const VotingList = ({ canManage, votingList, channelLogin }: Props) => {
+  return (
+    <Grid container>
+      {votingList.map((voting) => (
+        <Grid item sx={{ mb: 2 }} width="100%" key={voting.id}>
+          <VotingCard
+            voting={voting}
+            canManage={canManage}
+            href={`/${channelLogin}/voting/${voting.id}`}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default VotingList;
