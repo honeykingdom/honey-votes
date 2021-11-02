c589187c5a3f444e22ed4368c258e45a7268aa10
DmitryScaletta <dmitryscaletta@mail.ru>
2021-11-02 14:41:03 +0300

    ✨ Add voting constants

---
+++ src/features/voting/votingConstants.ts
@@ -0,0 +1,10 @@
+export enum FollowedTime {
+  None = 0,
+  TenMinutes = 10,
+  ThirtyMinutes = 30,
+  OneHour = 60,
+  OneDay = 60 * 24,
+  OneWeek = 60 * 24 * 7,
+  OneMonth = 60 * 24 * 30,
+  ThreeMonths = 60 * 24 * 30 * 3,
+}
