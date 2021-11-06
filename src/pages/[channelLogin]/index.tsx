86d828e2a5ccc64d6958f29f5cd2117b01be8bfd
DmitryScaletta <dmitryscaletta@mail.ru>
2021-11-06 14:20:00 +0300

    ✨ Update channel page

--- src/pages/[channelLogin]/index.tsx
+++ src/pages/[channelLogin]/index.tsx
@@ -1,17 +1,43 @@
+import NextLink from "next/link";
+import { Box, Divider, Link, Typography } from "@mui/material";
+import Breadcrumbs from "components/Breadcrumbs";
 import Layout from "components/Layout";
-// import UserCard from "components/UserCard";
-import { useUserQuery } from "features/api/apiSlice";
+import { useMeQuery, useUserQuery } from "features/api/apiSlice";
 import useChannelLogin from "hooks/useChannelLogin";
 
 const ChannelPage = () => {
   const login = useChannelLogin();
 
+  const me = useMeQuery();
   const channel = useUserQuery({ login }, { skip: !login });
 
   return (
     <Layout>
-      {/* <pre>{JSON.stringify(channel, null, 2)}</pre> */}
-      {/* <UserCard login={login} /> */}
+      <Box sx={{ mb: 2 }}>
+        <Typography component="div" variant="h4">
+          {channel.data?.displayName || login}
+        </Typography>
+      </Box>
+
+      {channel.data && (
+        <Box sx={{ mb: 2 }}>
+          <Breadcrumbs
+            items={[{ title: channel.data?.displayName || login }]}
+          />
+        </Box>
+      )}
+
+      <Divider sx={{ mb: 2 }} />
+
+      <NextLink href={`/${me.data.login}/voting`} passHref>
+        <Link>Голосование</Link>
+      </NextLink>
+
+      <br />
+
+      <NextLink href={`/${me.data.login}/chat-voting`} passHref>
+        <Link>Голосование в чате</Link>
+      </NextLink>
     </Layout>
   );
 };
