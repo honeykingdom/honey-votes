17475d58ebc753dcbc3079ff59d63c2da94c8455
DmitryScaletta <dmitryscaletta@mail.ru>
2021-11-13 15:25:54 +0300

    🔧 Enable experimental externalDir flag

--- chat-goal-widget/next.config.js
+++ chat-goal-widget/next.config.js
@@ -3,4 +3,8 @@ module.exports = {
   reactStrictMode: true,
   assetPrefix:
     process.env.NODE_ENV === "development" ? undefined : "/chat-goal-widget",
+  experimental: {
+    // https://github.com/vercel/next.js/issues/9474#issuecomment-810212174
+    externalDir: true,
+  },
 };
