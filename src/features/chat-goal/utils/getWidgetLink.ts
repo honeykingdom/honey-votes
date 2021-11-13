41dce7ffd3d4634441c39f1783a2a34d3ffe6857
DmitryScaletta <dmitryscaletta@mail.ru>
2021-11-13 15:59:42 +0300

    🩹 Add widget link for development

--- src/features/chat-goal/utils/getWidgetLink.ts
+++ src/features/chat-goal/utils/getWidgetLink.ts
@@ -1,4 +1,8 @@
 const getWidgetLink = (id: string) => {
+  if (process.env.NODE_ENV === "development") {
+    return `http://localhost:3002/?id=${id}`;
+  }
+
   const origin =
     (typeof window !== "undefined" && window.location.origin) || "";
 
