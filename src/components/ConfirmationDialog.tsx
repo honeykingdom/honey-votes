ec90fcc51a628bbc954915eb83dd94f523ccbf2a
DmitryScaletta <dmitryscaletta@mail.ru>
2021-12-06 21:50:31 +0300

    🩹 Translate confirmation dialog component

--- src/components/ConfirmationDialog.tsx
+++ src/components/ConfirmationDialog.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import {
   Button,
   Dialog,
@@ -21,32 +22,36 @@ const ConfirmationDialog = ({
   description,
   handleYes,
   handleClose,
-}: Props) => (
-  <Dialog
-    open={open}
-    onClose={handleClose}
-    aria-labelledby="alert-dialog-title"
-    aria-describedby="alert-dialog-description"
-  >
-    <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
-    <DialogContent>
-      <DialogContentText id="alert-dialog-description">
-        {description}
-      </DialogContentText>
-    </DialogContent>
-    <DialogActions>
-      <Button onClick={handleClose}>Нет</Button>
-      <Button
-        onClick={() => {
-          handleYes();
-          handleClose();
-        }}
-        autoFocus
-      >
-        Да
-      </Button>
-    </DialogActions>
-  </Dialog>
-);
+}: Props) => {
+  const [t] = useTranslation();
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {description}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>{t('no')}</Button>
+        <Button
+          onClick={() => {
+            handleYes();
+            handleClose();
+          }}
+          autoFocus
+        >
+          {t('yes')}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
 
 export default ConfirmationDialog;
