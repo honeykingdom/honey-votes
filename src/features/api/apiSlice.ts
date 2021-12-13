b71e1ffcf39b0a0f67473bc0d4fc18fb5c5558f8
DmitryScaletta <dmitryscaletta@mail.ru>
2021-12-13 19:15:57 +0300

    Revert "🩹 Temporary listen to all voting options table"
    This reverts commit 991b23510c0469895f51f3aa50d7c158b592c36d.

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -169,17 +169,11 @@ export const api = createApi({
         try {
           await cacheDataLoaded;
 
-          // TODO: `${VOTING_OPTION_TABLE_NAME}:votingId=eq.${arg}`
           subscription = supabase
-            .from<VotingOption>(VOTING_OPTION_TABLE_NAME)
-            .on('*', (payload) => {
-              if (
-                payload.new?.votingId !== arg &&
-                payload.old?.votingId !== arg
-              ) {
-                return;
-              }
-
+            .from<VotingOption>(
+              `${VOTING_OPTION_TABLE_NAME}:votingId=eq.${arg}`,
+            )
+            .on('*', (payload) =>
               updateCachedData((draft) => {
                 if (payload.eventType === 'INSERT') {
                   votingOptionsAdapter.addOne(draft, payload.new);
@@ -195,8 +189,8 @@ export const api = createApi({
                 if (payload.eventType === 'DELETE') {
                   votingOptionsAdapter.removeOne(draft, payload.old.id);
                 }
-              });
-            })
+              }),
+            )
             .subscribe();
         } catch {}
 
