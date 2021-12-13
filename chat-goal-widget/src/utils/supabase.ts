06d4ae5fbacd96c46531afaf8fa57e2b458b05ae
DmitryScaletta <dmitryscaletta@mail.ru>
2021-12-13 18:59:16 +0300

    🩹 Add access token for supabase in widget

--- chat-goal-widget/src/utils/supabase.ts
+++ chat-goal-widget/src/utils/supabase.ts
@@ -1,8 +1,10 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_HOST as string,
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_ANON_KEY as string
+  process.env.NEXT_PUBLIC_SUPABASE_HOST!,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_ANON_KEY!,
 );
+
+supabase.auth.setAuth(process.env.NEXT_PUBLIC_SUPABASE_REALTIME_USER_KEY!);
 
 export default supabase;
