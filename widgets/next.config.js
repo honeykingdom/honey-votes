0fc4099695406c2d84a311d9e1f1c086bceb97ec
DmitryScaletta <dmitryscaletta@mail.ru>
2022-03-13 14:52:59 +0300

    🔧 Update assetPrefix

--- widgets/next.config.js
+++ widgets/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  assetPrefix:
-    process.env.NODE_ENV === "development" ? undefined : "/chat-goal-widget",
+  assetPrefix: process.env.NODE_ENV === 'development' ? undefined : '/widgets',
   experimental: {
     // https://github.com/vercel/next.js/issues/9474#issuecomment-810212174
     externalDir: true,
